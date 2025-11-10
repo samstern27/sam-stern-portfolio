ebbfa051e666c3093d079d65c667abf19f891a65
Sam Stern <stern7@outlook.com>
2025-11-10 14:14:29 +0000

    Refactored ProjectCard.tsx
    Refactored mobile, sm:, md: sections into one

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,7 +3,7 @@
 import { CardData } from "@/types";
 import Image from "next/image";
 
-export default function ProjectCard({
+export default function ProjectCardB({
   id,
   name,
   github,
@@ -20,113 +20,33 @@ export default function ProjectCard({
 
   return (
     <div className="project-card">
-      {/* Medium/Large Screen */}
       <div
-        className="hidden md:flex glowing-border w-full h-70 flex-row  md:hover:scale-103 duration-500 cursor-pointer"
-        onClick={() => handleCardClick(id !== 3 ? vercel : github)}
+        className="flex md:flex-row glowing-border w-90 sm:w-120 md:w-full h-130 sm:h-140 md:h-70 flex-col px-3.5 sm:px-0 pt-4 sm:pt-0 md:hover:scale-103 duration-500"
+        onClick={() => handleCardClick(id === 3 ? github : vercel)}
         popoverTarget="_blank"
       >
         <div
-          className="flex flex-row justify-evenly w-full ml-5 pr-7 items-center
-         gap-5 rounded-t-xl"
+          className="flex flex-col md:flex-row justify-start md:justify-evenly w-full sm:mt-4 md:mt-0 md:ml-5 md:pr-7 items-center
+                 gap-5 sm:rounded-t-xl"
         >
           <Image
             src={imageSrc}
             width={width}
             height={height}
             alt={alt}
-            className="w-60 h-60 object-cover rounded-l-lg [mask-image:linear-gradient(to_right,black_0%,transparent_100%)] [mask-size:100%_100%] [mask-repeat:no-repeat]"
+            className="max-w-80 sm:max-w-none w-auto sm:w-110 md:w-60 md:h-60 sm:object-cover rounded-t-lg md:rounded-t-none md:rounded-l-lg [mask-image:linear-gradient(to_bottom,black_0%,transparent_100%)] md:[mask-image:linear-gradient(to_right,black_0%,transparent_100%)] [mask-size:100%_100%] [mask-repeat:no-repeat]"
           />
           <div className="flex w-full flex-row justify-between items-center">
-            <div className="flex flex-col justify-center items-left h-auto pr-15 w-full gap-3">
-              <h2 className="text-3xl md:text-3xl w-40 font-semibold tracking-tight text-left text-white">
+            <div className="flex flex-col justify-center items-center md:items-start h-auto w-full gap-3 md:pr-15">
+              <h2 className="text-3xl w-40 font-semibold tracking-tight text-center md:text-left text-white">
                 {name}
               </h2>
-              <p className="text-sm/7 text-left max-w-60 text-gray-400 ">
-                {brief}
-              </p>
-            </div>
-            <div className="flex flex-col justify-center gap-5 items-center my-5">
-              <a
-                href={github}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={(e) => e.stopPropagation()}
-                className="relative flex group"
-                title="Github"
-              >
-                <span
-                  className="
-                  absolute right-13 bottom-5 translate-y-1/2 
-                  text-neutral-500 text-xs
-                  md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
-                "
-                >
-                  GitHub
-                </span>
-                <Image
-                  src="/gh.svg"
-                  width={64}
-                  height={64}
-                  alt="Github logo"
-                  className="w-10 flex-shrink-0 transform transition-transform duration-300 hover:scale-110"
-                />
-              </a>
-              {id !== 3 ? (
-                <a
-                  href={vercel}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={(e) => e.stopPropagation()}
-                  className="relative flex group"
-                  title="Vercel"
-                >
-                  <Image
-                    src="/vercel.svg"
-                    width={683}
-                    height={683}
-                    alt="Vercel logo"
-                    className="w-10 flex-shrink-0 transform transition-transform duration-300 hover:scale-110"
-                  />
-                  <span
-                    className="
-                  absolute  right-13 bottom-5 translate-y-1/2 
-                  text-neutral-500 text-xs  
-                  md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
-                "
-                  >
-                    Vercel
-                  </span>
-                </a>
-              ) : null}
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* Small Screen */}
-      <div className="hidden sm:flex md:hidden glowing-border w-120 h-140 flex-row  md:hover:scale-103 duration-500">
-        <div
-          className="flex flex-col justify-start w-full mt-4 items-center
-         gap-5 rounded-t-xl"
-        >
-          <Image
-            src={imageSrc}
-            width={width}
-            height={height}
-            alt={alt}
-            className="w-110 object-cover rounded-t-lg [mask-image:linear-gradient(to_bottom,black_0%,transparent_100%)] [mask-size:100%_100%] [mask-repeat:no-repeat]"
-          />
-          <div className="flex w-full flex-row justify-between items-center">
-            <div className="flex flex-col justify-center items-center h-auto w-full gap-3 ">
-              <h2 className="text-3xl md:text-3xl w-40 font-semibold tracking-tight text-center text-white">
-                {name}
-              </h2>
-              <p className="text-sm/7 text-center max-w-90 text-gray-400 ">
+              <p className="text-sm/7 text-center md:text-left max-w-60 sm:max-w-90 text-gray-400">
                 {brief}
               </p>
             </div>
           </div>
-          <div className="flex flex-crow justify-center gap-5 items-center my-5">
+          <div className="flex flex-row md:flex-col justify-center gap-5 items-center my-5">
             <a
               href={github}
               target="_blank"
@@ -137,10 +57,10 @@ export default function ProjectCard({
             >
               <span
                 className="
-                  absolute right-12 bottom-5 translate-y-1/2 
-                  text-neutral-500 text-xs
-                  md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
-                "
+                          absolute right-12 md:right-13 bottom-5 translate-y-1/2 
+                          text-neutral-500 text-xs
+                          md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
+                        "
               >
                 GitHub
               </span>
@@ -170,89 +90,10 @@ export default function ProjectCard({
                 />
                 <span
                   className="
-                  absolute  left-12 bottom-5 translate-y-1/2 
-                  text-neutral-500 text-xs  
-                  md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
-                "
-                >
-                  Vercel
-                </span>
-              </a>
-            ) : null}
-          </div>
-        </div>
-      </div>
-      {/* Mobile Screen */}
-      <div className="sm:hidden flex glowing-border w-90 h-130 flex-row px-3.5 pt-4 md:hover:scale-103 duration-500">
-        <div
-          className="flex flex-col justify-start w-full items-center
-         gap-5"
-        >
-          <Image
-            src={imageSrc}
-            width={width}
-            height={height}
-            alt={alt}
-            className="max-w-80 w-auto rounded-t-lg [mask-image:linear-gradient(to_bottom,black_0%,transparent_100%)] [mask-size:100%_100%] [mask-repeat:no-repeat]"
-          />
-          <div className="flex w-full flex-row justify-between items-center">
-            <div className="flex flex-col justify-center items-center h-auto w-full gap-3 ">
-              <h2 className="text-3xl md:text-3xl w-40 font-semibold tracking-tight text-center text-white">
-                {name}
-              </h2>
-              <p className="text-sm/7 text-center max-w-60 text-gray-400 ">
-                {brief}
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-crow justify-center gap-5 items-center my-5">
-            <a
-              href={github}
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={(e) => e.stopPropagation()}
-              className="relative flex group"
-              title="Github"
-            >
-              <span
-                className="
-                  absolute right-12 bottom-5 translate-y-1/2 
-                  text-neutral-500 text-xs
-                  md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
-                "
-              >
-                GitHub
-              </span>
-              <Image
-                src="/gh.svg"
-                width={64}
-                height={64}
-                alt="Github logo"
-                className="w-10 flex-shrink-0 transform transition-transform duration-300 hover:scale-110"
-              />
-            </a>
-            {id !== 3 ? (
-              <a
-                href={vercel}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={(e) => e.stopPropagation()}
-                className="relative flex group"
-                title="Vercel"
-              >
-                <Image
-                  src="/vercel.svg"
-                  width={683}
-                  height={683}
-                  alt="Vercel logo"
-                  className="w-10 flex-shrink-0 transform transition-transform duration-300 hover:scale-110"
-                />
-                <span
-                  className="
-                  absolute  left-12 bottom-5 translate-y-1/2 
-                  text-neutral-500 text-xs  
-                  md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
-                "
+                          absolute left-12 md:left-auto md:right-13 bottom-5 translate-y-1/2 
+                          text-neutral-500 text-xs  
+                          md:opacity-0 md:group-hover:opacity-100  md:hover:hidden md:transition-opacity md:duration-300
+                        "
                 >
                   Vercel
                 </span>
